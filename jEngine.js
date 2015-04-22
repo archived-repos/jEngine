87d2a5f737d09b0ec4be382961867a9221606db5
Jesús Germade <jesus@germade.es>
2015-04-22 09:03:08 +0200

    increasing version

--- jEngine.js
+++ jEngine.js
@@ -2265,3 +2265,380 @@
 
     return Scope;
 });
+
+
+/*  ----------------------------------------------------------------------------------------- */
+
+/*
+ * css.js
+ *
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2014 Jesús Manuel Germade Castiñeiras <jesus@germade.es>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+
+(function (definition, root) {
+	'use strict';
+	
+	if ( typeof root === 'undefined' ) {
+		if ( typeof module !== 'undefined' ) {
+			module.exports = definition();
+		}
+	} else {
+		if ( root.fn ) {
+			fn.define('_', definition );
+		} else if( !root._ ) {
+			root._ = definition();
+		}
+	}
+
+})(function () {
+	'use strict';
+
+    function _extend () {
+        var auxArray = [],
+            dest = auxArray.shift.call(arguments),
+            src = auxArray.shift.call(arguments),
+            key;
+
+        while( src ) {
+            for( key in src ) {
+                dest[key] = src[key];
+            }
+            src = auxArray.shift.call(arguments);
+        }
+
+        return dest;
+    }
+
+	function _isType (type) {
+        return function (o) {
+            return (typeof o === type);
+        };
+    }
+
+    function _instanceOf (_constructor) {
+        return function (o) {
+            return ( o instanceof _constructor );
+        };
+    }
+
+    function key (o, fullKey, value){
+        if(! o instanceof Object) return false;
+        var oKey, keys = fullKey.split('.');
+        if(value !== undefined) {
+            if(keys.length) {
+                oKey = keys.shift();
+                var nextKey = keys.shift();
+                while( nextKey ) {
+                    if( !o[oKey] ) o[oKey] = {};
+                    o = o[oKey];
+                    oKey = nextKey;
+                    nextKey = keys.shift();
+                }
+                o[oKey] = value;
+                return value;
+            }
+            return false;
+        } else {
+            for(var k = 0, len = keys.length, inKeys = o || {}; k < len ; k++ ) {
+                oKey = keys[k];
+                if( oKey in inKeys ) inKeys = inKeys[keys[k]] || {};
+                else return false;
+            }
+            return inKeys;
+        }
+    }
+
+    function extend () {
+        if( arguments.length > 1 ) {
+            var target = [].shift.call(arguments), o = [].shift.call(arguments);
+
+            while( o ) {
+                _extend(target, o);
+                o = [].shift.call(arguments);
+            }
+        }
+    }
+
+
+    var RE_$$ = /^\$\$/,
+        auxArray = [];
+
+        function _extendByType (orig, sanitize) {
+            if( orig instanceof Array ) {
+                return _deepExtend([], orig, sanitize);
+            } else if( orig instanceof Object ) {
+                return _deepExtend({}, orig, sanitize);
+            }
+            return orig;
+        }
+
+        function _deepExtend (dest, orig, sanitize) {
+
+            if( dest === undefined ) {
+
+                return _extendByType(orig);
+
+            }
+
+            if( orig instanceof Array ) {
+
+                if( !dest instanceof Array ) {
+                    return _deepExtend([], orig, sanitize);
+                }
+
+                for( var i = 0, len = orig.length; i < len; i++ ) {
+
+                    if( dest[i] ) {
+                        dest[i] = _deepExtend(dest[i], orig[i], sanitize);
+                    } else {
+                        dest.push(_extendByType(orig[i]));
+                    }
+                }
+
+            } else if( orig instanceof Object ) {
+
+                if( !dest instanceof Object ) {
+                    return _deepExtend({}, orig, sanitize);
+                }
+
+                for( var key in orig ) {
+                    if( !sanitize || !RE_$$.test(key) ) {
+
+                        dest[key] = _deepExtend(dest[key], orig[key], sanitize);
+                    }
+                }
+            }
+
+            return dest;
+        }
+
+        function deepExtend () {
+
+            if( arguments.length < 2 ) {
+                return arguments[0];
+            }
+
+            var first = auxArray.shift.apply(arguments),
+                next = auxArray.shift.apply(arguments);
+
+            while( next ) {
+                _deepExtend(first, next);
+                next = auxArray.shift.apply(arguments);
+            }
+
+            return first;
+        }
+
+        function sanitize (obj) {
+
+            return _deepExtend({}, obj, true);
+        }
+
+
+    function _proccessPipe (pipe, args) {
+        var result = pipe[0].apply(null, args);
+
+        for( var i = 1, len = pipe.length; i < len; i++ ) {
+            result = pipe[i](result);
+        }
+
+        return result;
+    }
+
+    function _addToPipe (pipe, args) {
+        for( var i = 0, len = args.length; i < len; i++ ) {
+            if( !args[i] instanceof Function ) {
+                throw 'only Functions are allowed as pipe arguments';
+            } else {
+                pipe.push(args[i]);
+            }
+        }
+    }
+
+    function _eachInList( list, iteratee, thisArg ) {
+        for( var i = 0, len = list.length; i < len ; i++ ) {
+            iteratee.apply(thisArg, [ list[i], i ]);
+        }
+    }
+
+    function _eachInObject( o, iteratee, thisArg ) {
+        for( var key in o ) {
+            iteratee.call(thisArg, [o[key], key]);
+        }
+    }
+
+    function each (o, iteratee, thisArg) {
+        if( o instanceof Array ) {
+            _eachInList(o, iteratee, thisArg);
+        } else if( o instanceof Object ) {
+            _eachInObject(o, iteratee, thisArg);
+        }
+    }
+
+    function indexOf (list, comparator) {
+        
+        if( comparator instanceof Function ) {
+            for( var i = 0, len = list.length; i < len; i++ ) {
+                if( comparator(list[i]) ) {
+                    return i;
+                }
+            }
+        } else return list.indexOf(comparator);
+
+        return -1;
+    }
+
+    function remove (list, comparator) {
+
+        var i, len;
+        
+        if( comparator instanceof Function ) {
+            for( i = 0, len = list.length; i < len; i++ ) {
+                if( comparator(list[i]) ) {
+                    list.splice(i, 1);
+                    i--;
+                }
+            }
+        } else {
+            for( i = 0, len = list.length; i < len; i++ ) {
+                if( list[i] === comparator ) {
+                    list.splice(i, 1);
+                    i--;
+                }
+            }
+        }
+    }
+
+    function matchAll (o, filters) {
+        for( var key in filters ) {
+            if( o[key] !== filters[key] ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function matchAny (o, filters) {
+        for( var key in filters ) {
+            if( o[key] === filters[key] ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function find (list, filters) {
+        for( var i = 0, len = list.length ; i < len ; i++ ) {
+            if( matchAll(list[i], filters) ) {
+                return list[i];
+            }
+        }
+
+        return false;
+    }
+
+    function filter (list, filters) {
+        var newList = [];
+
+        for( var i = 0, len = list.length ; i < len ; i++ ) {
+            if( matchAll(list[i], filters) ) {
+                newList.push(list[i]);
+            }
+        }
+
+        return newList;
+    }
+
+    var _Funcs = {
+		isFunction: _isType('function'),
+        isString: _isType('string'),
+        isNumber: _isType('number'),
+        isArray: _instanceOf(Array),
+        isDate: _instanceOf(Date),
+        isRegExp: _instanceOf(RegExp),
+		isObject: function (myVar,type){ if( myVar instanceof Object ) return ( type === 'any' ) ? true : ( typeof myVar === (type || 'object') ); else return false; },
+
+		key: key,
+    	keys: Object.keys,
+
+    	extend: extend,
+        matchAll: matchAll,
+        matchAny: matchAny,
+        find: find,
+        filter: filter,
+
+        sanitize: sanitize,
+        merge: deepExtend,
+        copy: function (o) {
+            if( o instanceof Array ) {
+                return deepExtend([], o);
+            }
+            return deepExtend({}, o);
+        },
+
+        each: each,
+        indexOf: indexOf,
+        remove: remove,
+
+        pipe: function () {
+            var pipe = [];
+
+            _addToPipe(pipe, arguments);
+
+            var pipeFn = function () {
+                return _proccessPipe(pipe, arguments);
+            };
+
+            pipeFn.pipe = function () {
+                _addToPipe(pipe, arguments);
+                return pipeFn;
+            };
+
+            return pipeFn;
+        },
+        chain: function (value) {
+            return new Chain(value);
+        }
+	};
+
+    function Chain (value) {
+        this.value = value;
+    }
+
+    _eachInList(['key', 'keys', 'each', 'indexOf', 'remove'], function (nameFn) {
+        Chain.prototype[nameFn] = function () {
+            [].unshift.call(arguments, this.value);
+            _[nameFn].apply(null, arguments);
+        };
+    });
+
+    function _ (value) {
+        return new Chain(value);
+    }
+
+    extend(_, _Funcs);
+
+	return _;
+
+}, this);
